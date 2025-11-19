ae52f323d4bb3ab0c3b57c936a6824cc444943ef
sreekesh-nexotech <sreekesh@nexotech.cc>
2025-11-19 23:25:49 +0530

    changed back app.dart file for merging to dev

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,42 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// import '../features/profile/presentation/screen/profile_screen.dart';
+// import 'router/app_router.dart';
+// import 'theme/theme.dart';
+
+// class App extends StatelessWidget {
+//   const App({super.key});
+
+//   static const AppRouter _router = AppRouter();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ScreenUtilInit(
+//       designSize: const Size(390, 835),
+//       minTextAdapt: true,
+//       splitScreenMode: true,
+//       builder: (context, _) {
+//         return MaterialApp(
+//           debugShowCheckedModeBanner: false,
+//           title: 'Grocery App',
+//           theme: AppTheme.light,
+//           darkTheme: AppTheme.light,
+//           themeMode: ThemeMode.system,
+//           // TEMPORARILY SHOWING PROFILE SCREEN FOR TESTING
+//           home: const ProfileScreen(),
+//           // Uncomment below to use router navigation
+//           // initialRoute: AppRouter.initialRoute,
+//           // onGenerateRoute: _router.onGenerateRoute,
+//         );
+//       },
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../features/profile/presentation/screen/profile_screen.dart';
 import 'router/app_router.dart';
 import 'theme/theme.dart';
 
@@ -13,7 +48,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(390, 835),
+      designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, _) {
@@ -23,11 +58,8 @@ class App extends StatelessWidget {
           theme: AppTheme.light,
           darkTheme: AppTheme.light,
           themeMode: ThemeMode.system,
-          // TEMPORARILY SHOWING PROFILE SCREEN FOR TESTING
-          home: const ProfileScreen(),
-          // Uncomment below to use router navigation
-          // initialRoute: AppRouter.initialRoute,
-          // onGenerateRoute: _router.onGenerateRoute,
+          initialRoute: AppRouter.initialRoute,
+          onGenerateRoute: _router.onGenerateRoute,
         );
       },
     );
