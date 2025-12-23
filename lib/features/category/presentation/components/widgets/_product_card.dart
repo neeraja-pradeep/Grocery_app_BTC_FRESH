925d28ecbc331d9bbdcc4a7e7b0e5971c045d0a9
Abin56 <abinjohn8089@gmail.com>
2025-12-23 21:49:58 +0530

    Dec 23  1 st commit address bug fixed  --@edwin

--- lib/features/category/presentation/components/widgets/_product_card.dart
+++ lib/features/category/presentation/components/widgets/_product_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import '../../../../../app/theme/app_spacing.dart';
 import '../../../../../app/theme/colors.dart';
 import '../../../../../core/network/socket_provider.dart';
